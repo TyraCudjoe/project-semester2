9212d077807d12c0bdd9be32c3d0045336d5a449
tyra cudjoe <tyra20015@gmail.com>
2021-02-17 15:55:16 +0100

    Type:"Addition + Styling" Change:"Added 3 compulsory pages + linked the pages to the links in the navbar" Why:"Without the linking of the pages, visitors would not be able to navigate trough the website. Therefore, the navbar would be useless"

--- navbar.php
+++ navbar.php
@@ -7,10 +7,10 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="#">Pricing</a>
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        <a class="nav-link active" aria-current="page" href="./index.php?content=home.php">Home</a>
+        <a class="nav-link" href="./index.php?content=aboutUs.php">Over ons</a>
+        <a class="nav-link" href="./index.php?content=form.php">Inschrijven</a>
+        <a class="nav-link" href="./index.php?content=contact.php" tabindex="-1">Contact</a>
       </div>
     </div>
   </div>
