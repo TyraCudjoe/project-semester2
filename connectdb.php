fb63598afa1ecf73e2d9179ace355c8db91784ec
tyra cudjoe <tyra20015@gmail.com>
2021-03-10 11:43:46 +0100

    added connection with the database + added cards to the home page

---
+++ connectdb.php
@@ -0,0 +1,10 @@
+<?php
+
+    define ("SERVERNAME", "localhost");
+    define("USERNAME", "root");
+    define("PASSWORD", "");
+    define("DBNAME", "inschrijvingen-autotechniek");
+    
+    $conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DBNAME);
+    
+?>
--- home.php
+++ home.php
@@ -1,9 +1,84 @@
 <div class="row">
-    <div class="col-">
+    <div class="col-12">
         <div class="card">
             <div class="card-body">
-               <p class="cardText">Welkom bij de opleiding <strong>Autotechniek</strong>.</p>
+                <p class="cardText">Welkom bij de opleiding <strong>Autotechniek</strong>.</p>
             </div>
         </div>
     </div>
 </div>
+
+<section id="section1">
+    <div class="row">
+        <div class="col-6">
+            <div class="card-body">
+                <p class="card-text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                </p>
+            </div>
+        </div>
+        <div class="col-6">
+            <img src="..." class="card-img-top" alt="...">
+        </div>
+    </div>
+</section>
+
+<section id="section1">
+    <div class="row">
+        <div class="col-6">
+            <img src="..." class="card-img-top" alt="...">
+        </div>
+        <div class="col-6">
+            <div class="card-body">
+                <p class="card-text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="section1">
+    <div class="row">
+        <div class="col-6">
+            <div class="card-body">
+                <p class="card-text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                </p>
+            </div>
+        </div>
+        <div class="col-6">
+            <img src="..." class="card-img-top" alt="...">
+        </div>
+    </div>
+</section>
+
+<section id="section1">
+    <div class="row">
+        <div class="col-6">
+            <img src="..." class="card-img-top" alt="...">
+        </div>
+        <div class="col-6">
+            <div class="card-body">
+                <p class="card-text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="section1">
+    <div class="row">
+        <div class="col-6">
+            <div class="card-body">
+                <p class="card-text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                </p>
+            </div>
+        </div>
+        <div class="col-6">
+            <img src="..." class="card-img-top" alt="...">
+        </div>
+    </div>
+</section>
