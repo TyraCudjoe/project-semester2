811d62b71b5ea49603c3f53b2b763edd05b6ff4e
AronVLD <aronridder@hotmail.nl>
2021-03-17 11:18:07 +0100

    added contacgt

---
+++ contact.php
@@ -0,0 +1,35 @@
+<div class = "container"> 
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <p class="cardText"><strong>Contact us!</strong></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <div class="card">
+                <img src="img/Daltonlaan-opti.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Foto</h5>
+                    <p class="card-text">Deze locatie is voor de opleiding autotechniek.</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">030 281 5100</li>
+                    <li class="list-group-item">mboutrecht@mbo.com</li>
+                    <li class="list-group-item">Daltonlaan 300, 3584 BK Utrecht</li>
+                </ul>
+                <div class="card-body">
+                    <a href="https://www.mboutrecht.nl/locaties/daltonlaan/" class="card-link">Website</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-6 mapSchool">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2451.5931112709604!2d5.158462715758956!3d52.08713647973455!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c668a2918b16e9%3A0x95fac93d2c242c97!2sDaltonlaan%20300%2C%203584%20BK%20Utrecht!5e0!3m2!1snl!2snl!4v1615975710316!5m2!1snl!2snl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+    </div>
+</div>
