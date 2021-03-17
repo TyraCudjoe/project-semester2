e518d78a1263765bb681ee5ee44584410ff0c991
tyra cudjoe <tyra20015@gmail.com>
2021-03-17 11:28:01 +0100

    added form to form.php

---
+++ form.php
@@ -0,0 +1,56 @@
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <form>
+                <p>Geslacht:</p>
+                <select class="form-select form-select-sm" aria-label=".form-select-sm example">
+                    <option selected>Kies een optie</option>
+                    <option value="1">Man</option>
+                    <option value="2">Vrouw</option>
+                    <option value="3">Zeg ik liever niet</option>
+                </select>
+                <div class="mb-3">
+                    <label for="exampleInputVoornaam" class="form-label">Voornaam:</label>
+                    <input type="voornaam" class="form-control" id="exampleInputVoornaam"
+                        aria-describedby="voornaamHelp">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputTussenvoegsel" class="form-label">Tussenvoegsel:</label>
+                    <input type="tussenvoegsel" class="form-control" id="exampleInputTussenvoegsel"
+                        aria-describedby="tussenvoegselHelp">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputAchternaam" class="form-label">Achternaam:</label>
+                    <input type="achternaam" class="form-control" id="exampleInputAchternaam"
+                        aria-describedby="achternaamHelp">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputGeboortedatum" class="form-label">Geboortedatum:</label>
+                    <input type="geboortedatum" class="form-control" id="exampleInputGeboortedatum"
+                        aria-describedby="geboortedatumHelp">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address:</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputTelefoonnummer1" class="form-label">Telefoonnummer:</label>
+                    <input type="telefoonnummer" class="form-control" id="exampleInputTelefoonnummer1">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputHuisnummer1" class="form-label">Huisnummer:</label>
+                    <input type="huisummer" class="form-control" id="exampleInputHuisnummer1">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPostcode1" class="form-label">Postcode:</label>
+                    <input type="Postcode" class="form-control" id="exampleInputPostcode1">
+                </div>
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
