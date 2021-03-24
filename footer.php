36296857ac5218f83a44f3eaa495cfc2a10b5f68
AronVLD <aronridder@hotmail.nl>
2021-03-24 12:19:30 +0100

    changed footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div class="footer">
     <div class="row">
         <div class="col-4">
-            <h3>Sitemap</h3>
+            <h3>Welkom bij onze website!</h3>
         </div>
         <div class="col-4"></div>
         <div class="col-4"></div>
