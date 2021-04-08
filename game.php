27e46cbce9e29e3d8fa9af62dfe1d993c86abe5a
AronVLD <aronridder@hotmail.nl>
2021-04-08 13:58:26 +0200

    oops

--- game.php
+++ game.php
@@ -1,5 +1,5 @@
 <object id="UnityObject" classid="clsid:444785F1-DE89-4295-863A-D46C3A781394"
-        width="420" height="750"
+        width="1500" height="750"
         codebase="http://webplayer.unity3d.com/download_webplayer/UnityWebPlayer.cab#version=2,0,0,0">
  <param name="unity3d" value="WebPlayer.unity3d" />
  <embed id="UnityEmbed" src="(----------->path<------------).unity3d" width="1500" height="750"
