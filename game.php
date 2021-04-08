24b586eb992e3b0b8c188f7756e3efe9f21e57e5
tyra cudjoe <tyra20015@gmail.com>
2021-04-08 14:13:34 +0200

    centered game embed

--- game.php
+++ game.php
@@ -1,7 +1,11 @@
-<object id="UnityObject" classid="clsid:444785F1-DE89-4295-863A-D46C3A781394"
-        width="1500" height="750"
-        codebase="http://webplayer.unity3d.com/download_webplayer/UnityWebPlayer.cab#version=2,0,0,0">
- <param name="unity3d" value="WebPlayer.unity3d" />
- <embed id="UnityEmbed" src="(----------->path<------------).unity3d" width="1500" height="750"
-  type="application/vnd.unity" pluginspage="http://www.unity3d.com/unity-web-player-2.x" />
- </object>
+<div class="container">
+        <div class="mt-5 UnityObject">
+                <object classid="clsid:444785F1-DE89-4295-863A-D46C3A781394" width="1500" height="750"
+                        codebase="http://webplayer.unity3d.com/download_webplayer/UnityWebPlayer.cab#version=2,0,0,0">
+                        <param name="unity3d" value="WebPlayer.unity3d" />
+                        <embed id="UnityEmbed" src="(----------->path<------------).unity3d" width="1500" height="750"
+                                type="application/vnd.unity"
+                                pluginspage="http://www.unity3d.com/unity-web-player-2.x" />
+                </object>
+        </div>
+</div>
